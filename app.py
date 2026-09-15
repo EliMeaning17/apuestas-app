34bf0fab13ffec2dd9604bcb810d67b2b6101ada
EliMeaning17 <167259000+EliMeaning17@users.noreply.github.com>
2026-09-14 23:00:59 -0500

    Actualizar app.py

--- app.py
+++ app.py
@@ -182,7 +182,7 @@ def mostrar_login():
                 tiene_numeros = any(c.isdigit() for c in nueva_pass)
                 tiene_especiales = not nueva_pass.isalnum()
                 
-                    if len(nueva_pass) < 6:
+                if len(nueva_pass) < 6:
             st.error("❌ Mínimo 6 caracteres.")
         elif not (tiene_letras and tiene_numeros):
             st.error("❌ Debe contener letras y números.")
